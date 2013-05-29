6ae0f78412ffbe3b4f4bae4780f5c5db85ed6c49
Ritha <ra.rodrigu10@gmail.com>
2013-05-29 14:51:48 +0200

    Version 1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+Blog 2012
+
+Proyecto de la asignatura SWCM 2012-2013
+
+Autores: Ritha Rodriguez Pintado y Carlos Perez Mejias
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+
+/*
+* GET home page.
+*/
+
+var contador = require('../public/javascripts/count');
+exports.index = function(req, res){
+  //res.render('index', { visitas: ('' + contador.getCount()) });
+  res.render('index', { visitas: req.number});
+};

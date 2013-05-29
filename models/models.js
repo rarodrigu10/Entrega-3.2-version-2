c7e402d02756b136c5d76dbc366f7af05caf350f
Ritha <ra.rodrigu10@gmail.com>
2013-05-29 19:40:57 +0200

    Version adaptada para heroku

--- models/models.js
+++ models/models.js
@@ -5,9 +5,19 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 // Para usar SQLite:
-var sequelize = new Sequelize(null, null, null,
-{dialect: "sqlite",
-storage: "blog.sqlite"});
+var sequelize = new Sequelize(process.env.DATABASE_NAME,
+                              process.env.DATABASE_USER,
+                              process.env.DATABASE_PASSWORD,
+								{ dialect: process.env.DATABASE_DIALECT,
+              				      protocol: process.env.DATABASE_PROTOCOL,
+              					  port: process.env.DATABASE_PORT,
+								  host: process.env.DATABASE_HOST,
+                                  storage: process.env.DATABASE_STORAGE,
+              					  omitNull: true});
+
+// var sequelize = new Sequelize(null, null, null,
+// {dialect: "sqlite",
+// storage: "blog.sqlite"});
 
 // Importar la definicion de la clase Post desde post.js.
 // Y que este modulo exporta la clase Post:
